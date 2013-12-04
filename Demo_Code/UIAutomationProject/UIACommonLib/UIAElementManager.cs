d45174705c52b1a1eca9ad11c9d0c599ca3498c8
ducchien.phan <ducchien.phan@gmail.com>
2013-12-04 16:22:02 +0000

    add demo code

---
+++ Demo_Code/UIAutomationProject/UIACommonLib/UIAElementManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Automation;
+
+namespace UIACommonLib
+{
+    public class UIAElementManager
+    {
+        private static UIAElementManager m_instance;
+        private UIAElementManager()
+        {
+        }
+
+        public static UIAElementManager GetInstance()
+        {
+            if (m_instance != null)
+            {
+                m_instance = new UIAElementManager();
+            }
+            return m_instance;
+        }
+
+        public string GetAllTopWindows()
+        {
+            string strResult = "";
+            AutomationElementCollection listTopWindows = AutomationElement.RootElement.FindAll(TreeScope.Children, Condition.TrueCondition);
+            foreach (AutomationElement window in listTopWindows)
+            {
+                strResult += window.Current.NativeWindowHandle + "|";
+            }
+            return strResult;
+        }
+    }
+}
