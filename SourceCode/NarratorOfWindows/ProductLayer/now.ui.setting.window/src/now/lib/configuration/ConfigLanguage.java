743855093f9f33c5b521541eaa89ebe2bb691458
ducchien.phan <ducchien.phan@gmail.com>
2014-01-11 05:30:04 +0000

    build xml read and write class build config language class

---
+++ SourceCode/NarratorOfWindows/ProductLayer/now.ui.setting.window/src/now/lib/configuration/ConfigLanguage.java
@@ -0,0 +1,128 @@
+package now.lib.configuration;
+
+import now.lib.utilities.FileXML;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class ConfigLanguage {
+	private static ConfigLanguage m_instance = null;
+	
+	private String m_fileConfig = ConfigDefine.FILE_CONFIG_LANGUAGE;
+	private FileXML m_xmlFile = null;
+	private Document m_xmlDocument = null;
+	
+	private ConfigLanguage(){
+		m_xmlFile = new FileXML(m_fileConfig);
+		m_xmlDocument = m_xmlFile.getXMLDocument();
+	};
+	
+	/**
+	 * Save config to xml file
+	 */
+	private void saveConfig(){
+		if(m_xmlFile != null)
+		{
+			m_xmlFile.writeXMLDocument();
+		}
+	}
+	
+	/**
+	 * Get single-ton instance
+	 * @return single-ton instance of ConfigLanguage
+	 */
+	public static ConfigLanguage getInstance(){
+		if(m_instance == null){
+			m_instance = new ConfigLanguage();
+		}
+		return m_instance;
+	}
+	
+	/**
+	 * Get output language
+	 * @return output language
+	 */
+	public String getOutputLanguage() {
+		String result = "";
+		if(m_xmlDocument != null){
+			Element root = m_xmlDocument.getDocumentElement();
+			if(root != null){
+				result = root.getElementsByTagName(ConfigDefine.LANGUAGE_CONFIG_OUTPUT).item(0).getTextContent();
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Get input language
+	 * @return input language
+	 */
+	public String getInputLanguage() {
+		String result = "";
+		if(m_xmlDocument != null){
+			Element root = m_xmlDocument.getDocumentElement();
+			if(root != null){
+				result = root.getElementsByTagName(ConfigDefine.LANGUAGE_CONFIG_INPUT).item(0).getTextContent();
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Get display language
+	 * @return display language
+	 */
+	public String getDisplayLanguage() {
+		String result = "";
+		if(m_xmlDocument != null){
+			Element root = m_xmlDocument.getDocumentElement();
+			if(root != null){
+				result = root.getElementsByTagName(ConfigDefine.LANGUAGE_CONFIG_DISPLAY).item(0).getTextContent();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Set output language
+	 * @param language language to save to config 
+	 */
+	public void setOutputLanguage(String language){
+		if(m_xmlDocument != null){
+			Element root = m_xmlDocument.getDocumentElement();
+			if(root != null){
+				root.getElementsByTagName(ConfigDefine.LANGUAGE_CONFIG_OUTPUT).item(0).setTextContent(language);
+			}
+		}
+		saveConfig();
+	}
+	
+	/**
+	 * Set input language
+	 * @param language language to save to config 
+	 */
+	public void setInputLanguage(String language){
+		if(m_xmlDocument != null){
+			Element root = m_xmlDocument.getDocumentElement();
+			if(root != null){
+				root.getElementsByTagName(ConfigDefine.LANGUAGE_CONFIG_INPUT).item(0).setTextContent(language);
+			}
+		}
+		saveConfig();
+	}
+
+	/**
+	 * Set display language
+	 * @param language language to save to config 
+	 */
+	public void setDisplayLanguage(String language)
+	{
+		if(m_xmlDocument != null){
+			Element root = m_xmlDocument.getDocumentElement();
+			if(root != null){
+				root.getElementsByTagName(ConfigDefine.LANGUAGE_CONFIG_DISPLAY).item(0).setTextContent(language);
+			}
+		}
+		saveConfig();
+	}
+}
---
+++ SourceCode/NarratorOfWindows/ProductLayer/now.ui.setting.window/src/now/lib/utilities/FileXML.java
@@ -0,0 +1,85 @@
+package now.lib.utilities;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+ 
+/**
+ * This class used to get XML document
+ * @author Chien.Phan
+ *
+ */
+public class FileXML {
+	private String m_fileName = "";
+	private File m_xmlFile = null;
+	private Document m_xmlDocument = null;
+	
+	public FileXML(String filePath)	{
+		m_fileName = filePath;
+		m_xmlFile = new File(m_fileName);
+		if(!m_fileName.isEmpty() && m_xmlFile.exists())
+		{
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = null;
+			try {
+				dBuilder = dbFactory.newDocumentBuilder();
+				m_xmlDocument = dBuilder.parse(m_xmlFile);
+			} catch (ParserConfigurationException | SAXException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//optional, but recommended
+			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			m_xmlDocument.getDocumentElement().normalize();
+		}
+	}
+	
+	/**
+	 * Get XML document from xml file
+	 * @return xml document, null if file name is incorrectly
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	public Document getXMLDocument() {
+		if(m_xmlDocument != null)
+		{
+			return m_xmlDocument;
+		}
+		return null;
+	}
+	
+	/**
+	 * Write xml document to file
+	 * @throws IOException
+	 * @throws TransformerException
+	 */
+	public void writeXMLDocument() {
+		// Use a Transformer for output
+		TransformerFactory tFactory = TransformerFactory.newInstance();
+		DOMSource source = new DOMSource(m_xmlDocument);
+		StreamResult result = new StreamResult(m_xmlFile);
+		
+		Transformer transformer = null;
+		try {
+			transformer = tFactory.newTransformer();
+			transformer.transform(source, result);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
