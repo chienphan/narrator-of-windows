02bc1b37e8f10ab751554c8ba418445e6d302082
letranvinhtri <letranvinhtri@8156b7cc-288d-5193-a0fb-266b84c42d27>
2014-01-17 14:01:07 +0000

    add NowHyperlink_UIA

--- SourceCode/NarratorOfWindows/bin/Debug_32/rundebug.bat
+++ SourceCode/NarratorOfWindows/bin/Debug_32/rundebug.bat
@@ -1,2 +1,2 @@
-F:\UIAutomation\narrator-of-windows\SourceCode\NarratorOfWindows\jre\jre-7u45-windows-i586\jre1.7.0_45\bin\java.exe -jar "now.ui.setting.window.jar"
+D:\Duan\jre\jre-7u45-windows-i586\jre1.7.0_45\bin\java.exe -jar "now.ui.setting.window.jar"
 pause
