f888e7da077b1b7baa82d849e1168e890a88f7ce
letranvinhtri <letranvinhtri@8156b7cc-288d-5193-a0fb-266b84c42d27>
2014-01-17 09:12:59 +0000

    add NowCheckBox_UIA.cpp NowCheckBox_UIA.h

---
+++ SourceCode/NarratorOfWindows/PhysicalLayer/plugin/now.plugin.uiautomation/controls/NowCheckBox_UIA.h
@@ -0,0 +1,35 @@
+#ifndef _NOW_CHECK_BOX_UIA_H_
+#define _NOW_CHECK_BOX_UIA_H_
+
+#include "NowDefine.h"
+#include "INowControl.h"
+#include "NowControl_UIA.h"
+
+class NowCheckBox_UIA : public NowControl_UIA
+{
+public:
+	NowCheckBox_UIA(string strSignature, string strControlType);
+
+	~NowCheckBox_UIA(void);
+
+	////virtual NOW_RESULT getControlType( string& strControlType );
+
+	//virtual NOW_RESULT getCaption( wstring& wstrCaption );
+
+	virtual NOW_RESULT getUIInformation( wstring& wstrHelpText );
+
+	//virtual NOW_RESULT getParent( INowControl*& ctrParent );
+
+	//virtual NOW_RESULT getChildren( list<INowControl*>& lstChildren );
+
+	//virtual NOW_RESULT getState( int& nState );
+
+	//virtual NOW_RESULT getSignature( string& strSignature );
+
+	//virtual NOW_RESULT getNowClass( string& strNowClass );
+
+	//virtual NOW_RESULT getToolTip( wstring& wstrToolTip);
+
+};
+
+#endif
