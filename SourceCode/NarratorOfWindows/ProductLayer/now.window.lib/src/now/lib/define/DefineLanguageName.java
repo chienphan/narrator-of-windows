36f43701d30f17281f7e2f9c8acf47ae3ca3125c
ducchien.phan <ducchien.phan@gmail.com>
2014-02-23 12:03:51 +0000

    + add missing file

---
+++ SourceCode/NarratorOfWindows/ProductLayer/now.window.lib/src/now/lib/define/DefineLanguageName.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package now.lib.define;
+
+/**
+ *
+ * @author Chien.Phan
+ */
+public class DefineLanguageName {
+    //This class will be remove when using UI to handle config
+    public static final String VIETNAMESE 	= "Vietnamese";
+    public static final String ENGLISH 		= "English";
+    public static final String FRENCH 		= "French";
+}
