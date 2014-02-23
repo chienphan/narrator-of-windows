736eb2b9b72555cda88b68c806ada7ec13856ca6
ducchien.phan <ducchien.phan@gmail.com>
2014-02-23 07:16:36 +0000

    + update system tray + create template for Configuration Window

--- SourceCode/NarratorOfWindows/ProductLayer/now.window.lib/src/now/lib/configuration/ConfigDefine.java
+++ SourceCode/NarratorOfWindows/ProductLayer/now.window.lib/src/now/lib/configuration/ConfigDefine.java
@@ -4,6 +4,7 @@ public class ConfigDefine {
 	public static final String FILE_CONFIG_LANGUAGE 				= "config\\config-language.xml";
 	public static final String FILE_CONFIG_LANGUAGE_CODE_GOOGLE 	= "config\\LanguageCodeGoogle.ini";
 	public static final String FILE_CONFIG_LANGUAGE_CODE_MICROSOFT 	= "config\\LanguageCodeMicrosoft.ini";
+        public static final String FILE_IMAGE_TRAY_ICON         = "resource\\tray-icon.gif";
 	public static final String LANGUAGE_CONFIG_INPUT 	= "inputlanguage";
 	public static final String LANGUAGE_CONFIG_OUTPUT 	= "outputlanguage";
 	public static final String LANGUAGE_CONFIG_DISPLAY 	= "displaylanguage";
