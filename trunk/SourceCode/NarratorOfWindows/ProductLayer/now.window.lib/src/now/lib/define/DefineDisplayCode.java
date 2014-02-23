ce49dd008f6c1428c8faf8f3c50e767760a418f5
ducchien.phan@gmail.com <ducchien.phan@gmail.com@8156b7cc-288d-5193-a0fb-266b84c42d27>
2014-02-23 12:03:51 +0000

    + add missing file

---
+++ trunk/SourceCode/NarratorOfWindows/ProductLayer/now.window.lib/src/now/lib/define/DefineDisplayCode.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package now.lib.define;
+
+/**
+ *
+ * @author Chien.Phan
+ */
+public class DefineDisplayCode {
+    public static final String LANGUAGE_NAME                = "LANGUAGE_NAME";
+    public static final String LANGUAGE_DISPLAY             = "LANGUAGE_DISPLAY";
+    public static final String SYSTEM_TRAY_CONFIGURATION    = "SYSTEM_TRAY_CONFIGURATION";
+    public static final String SYSTEM_TRAY_EXIT             = "SYSTEM_TRAY_EXIT";
+}
