5279ce2eee0bb81a4d0e3b5a000020a7129b2b73
ducchien.phan <ducchien.phan@gmail.com>
2014-03-15 10:38:26 +0000

    - add JNativeHook.jar - update debug file

--- SourceCode/NarratorOfWindows/bin/Debug_32/rundebug.bat
+++ SourceCode/NarratorOfWindows/bin/Debug_32/rundebug.bat
@@ -1,2 +1,9 @@
-D:\Duan\jre\jre-7u45-windows-i586\jre1.7.0_45\bin\java.exe -jar "now.ui.setting.window.jar"
+REM Copy all jar libraries into debug directory...
+set JAVA_BUILD_DIRECTORY=F:\UIAutomation\narrator-of-windows\SourceCode\NarratorOfWindows\ProductLayer\now.window.lib\dist
+set DEBUG_DIRECTORY=F:\UIAutomation\narrator-of-windows\SourceCode\NarratorOfWindows\bin\Debug_32
+if exist %JAVA_BUILD_DIRECTORY% xcopy %JAVA_BUILD_DIRECTORY% %DEBUG_DIRECTORY% /E /Y
+
+REM Call jar entry point...
+F:\UIAutomation\narrator-of-windows\SourceCode\NarratorOfWindows\jre\jre-7u45-windows-i586\jre1.7.0_45\bin\java.exe -jar "now.window.lib.jar"
+
 pause
