680ab0a565f5b3e7b0c308ea1d6f9f9de3c9804f
ducchien.phan@gmail.com <ducchien.phan@gmail.com@8156b7cc-288d-5193-a0fb-266b84c42d27>
2014-03-31 09:53:00 +0000

    - modify condition of keep window to storage

--- trunk/SourceCode/NarratorOfWindows/LogicalLayer/now.core/NowStorage.cpp
+++ trunk/SourceCode/NarratorOfWindows/LogicalLayer/now.core/NowStorage.cpp
@@ -20,28 +20,30 @@ NowStorage* NowStorage::getInstance()
 	return m_instance;
 }
 
-NOW_RESULT NowStorage::keepWindowToStorage( const char* szWindowTitle, INowWindow* pwindow )
+NOW_RESULT NowStorage::keepWindowToStorage( const char* szWindowName, INowWindow* pwindow )
 {
-	if (szWindowTitle != NULL && pwindow != NULL)
+	//if pWindow == NULL, we also add to storage, when we use it later, 
+	//we will check and matching it again
+	if (szWindowName != NULL)
 	{
-		string strWindowTitle = string(szWindowTitle);
-		NowMapWindow::iterator it = m_mapWindow->find(strWindowTitle);
+		string strWindowTitle = string(szWindowName);
+		NowMapWindow::iterator it = m_mapWindow->find(szWindowName);
 		//check window is keep in map or not
 		if (it == m_mapWindow->end())
 		{
-			m_mapWindow->insert(pair<string, INowWindow*>(strWindowTitle, pwindow));
+			m_mapWindow->insert(pair<string, INowWindow*>(szWindowName, pwindow));
 			return NOW_OK;
 		}
 	}
 	return NOW_FALSE;
 }
 
-NOW_RESULT NowStorage::getWindowFromStorage( const char* szWindowTitle, INowWindow*& pWindow )
+NOW_RESULT NowStorage::getWindowFromStorage( const char* szWindowName, INowWindow*& pWindow )
 {
-	if (szWindowTitle != NULL)
+	if (szWindowName != NULL)
 	{
-		string strWindowTitle = string(szWindowTitle);
-		NowMapWindow::iterator it = m_mapWindow->find(strWindowTitle);
+		string strWindowTitle = string(szWindowName);
+		NowMapWindow::iterator it = m_mapWindow->find(szWindowName);
 		if (it != m_mapWindow->end())
 		{
 			pWindow = it->second;
