67984e2808e24b929db20cc6b91f9c90c60a886f
ducchien.phan <ducchien.phan@gmail.com>
2014-04-09 16:39:58 +0000

    - complete refactor new structure of UIA agent lib

---
+++ SourceCode/NarratorOfWindows/PhysicalLayer/agent/now.agent.uiautomation.client/action/common/BringWindowToTop.cs
@@ -0,0 +1,45 @@
+﻿using now.agent.uiautomation.client.interfaces;
+using now.agent.uiautomation.client.constant;
+using now.agent.uiautomation.client.logger;
+using now.agent.uiautomation.client.storage;
+using System.Windows.Automation;
+using now.agent.uiautomation.client.argument;
+using System;
+
+namespace now.agent.uiautomation.client.action.common
+{
+    class BringWindowToTop : INowUIAAction
+    {
+        public int DoAction()
+        {
+            int nResult = NowUIADefine.NOW_FALSE;
+
+            String strWindowSignature = NowUIAArgumentIn.GetInstance().GetStringArgument(NowUIAPropertyDefine.UIA_PROP_SIGNATURE);
+
+            AutomationElement window = NowUIAStorageAction.GetInstance().GetUIObjectFormCache(strWindowSignature.Trim());
+            //NowUIALogger.GetInstance().LogInfor("[BringWindowToTop] GetUIObjectFormCache");
+            if (window != null)
+            {
+                //NowUIALogger.GetInstance().LogInfor("[BringWindowToTop] window: not NULL");
+                object obj = window.GetCurrentPropertyValue(AutomationElement.IsWindowPatternAvailableProperty);
+                if (obj != null)
+                {
+                    bool hasWindowPattern = (bool)obj;
+                    if (hasWindowPattern == true)
+                    {
+                        //NowUIALogger.GetInstance().LogInfor("[BringWindowToTop] hasWindowPattern");
+                        obj = window.GetCurrentPattern(WindowPattern.Pattern);
+                        if (obj != null)
+                        {
+                            WindowPattern windowPattern = obj as WindowPattern;
+                            windowPattern.SetWindowVisualState(WindowVisualState.Normal);
+                            nResult = NowUIADefine.NOW_OK;
+                        }
+                    }
+                }
+            }
+
+            return nResult;
+        }
+    }
+}
---
+++ SourceCode/NarratorOfWindows/PhysicalLayer/agent/now.agent.uiautomation.client/action/common/GetWindowByTitle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Automation;
+using now.agent.uiautomation.client.argument;
+using now.agent.uiautomation.client.constant;
+using now.agent.uiautomation.client.interfaces;
+using now.agent.uiautomation.client.logger;
+using now.agent.uiautomation.client.storage;
+using now.agent.uiautomation.client.utility;
+
+namespace now.agent.uiautomation.client.action.common
+{
+    class GetWindowByTitle : INowUIAAction
+    {
+        public int DoAction()
+        {
+            int nResult = NowUIADefine.NOW_FALSE;
+
+            String strTitleWindow = NowUIAArgumentIn.GetInstance().GetStringArgument(NowUIAPropertyDefine.UIA_PROP_WINDOW_TITLE);
+
+            AutomationElement currentWindow = null;
+
+            //check for regular expression
+            if (NowUIAUtilities.GetInstance().IsRegExpPattern(strTitleWindow))
+            {
+                strTitleWindow = NowUIAUtilities.GetInstance().TrimRegExpPattern(strTitleWindow);
+            }
+
+            //get all windows
+            List<AutomationElement> listWindow = new List<AutomationElement>();
+            TreeWalker rawView = TreeWalker.RawViewWalker;
+            currentWindow = rawView.GetFirstChild(AutomationElement.RootElement);
+
+            while (currentWindow != null)
+            {
+                //NowUIALogger.GetInstance().LogInfor(">>>" + currentWindow.Current.Name);
+                listWindow.Add(currentWindow);
+                currentWindow = rawView.GetNextSibling(currentWindow);
+            }
+
+            //find the window has strTitleWindow
+            foreach (AutomationElement window in listWindow)
+            {
+                if (Regex.IsMatch(window.Current.Name, strTitleWindow))
+                {
+                    //NowUIALogger.GetInstance().LogInfor("IsMatch:" + window.Current.Name + "|" + window.Current.NativeWindowHandle);
+                    currentWindow = window;
+                    nResult = NowUIADefine.NOW_OK;
+                    break;
+                }
+            }
+
+            if (nResult == NowUIADefine.NOW_OK)
+            {
+                //keep the window element to cache and return strHandleWindow
+                String strHandleWindow = String.Format("{0}", currentWindow.Current.NativeWindowHandle);
+                NowUIAArgumentOut.GetInstance().Data = strHandleWindow;
+                NowUIAStorageAction.GetInstance().AddToCache(strHandleWindow, currentWindow);
+            }
+
+            return nResult;
+        }
+    }
+}
