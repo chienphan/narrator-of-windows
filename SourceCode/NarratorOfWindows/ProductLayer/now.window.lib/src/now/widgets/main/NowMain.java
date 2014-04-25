6d91990e626522d50679e09d1526534130a74c79
ducchien.phan <ducchien.phan@gmail.com>
2014-04-25 04:26:53 +0000

    support call exe form java and passing argument

--- SourceCode/NarratorOfWindows/ProductLayer/now.window.lib/src/now/widgets/main/NowMain.java
+++ SourceCode/NarratorOfWindows/ProductLayer/now.window.lib/src/now/widgets/main/NowMain.java
@@ -6,7 +6,12 @@
 
 package now.widgets.main;
 
-import now.lib.tcp.NowServerSocket;
+//import now.lib.tcp.NowServerSocket;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
@@ -18,15 +23,23 @@ public class NowMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Thread notificationThread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                NowServerSocket.getInstance().startListening();
-            }
-        });
-        notificationThread.start();
+        
+            //        Thread notificationThread = new Thread(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                NowServerSocket.getInstance().startListening();
+//            }
+//        });
+//        notificationThread.start();
                 
-        NowSystemTray.getInstance().showSystemTray();
+//        NowSystemTray.getInstance().showSystemTray();
+        String exePath = "f:\\My_Data\\narrator-of-windows\\SourceCode\\NarratorOfWindows\\bin\\Debug_32\\now.play.exe";
+        String arg = "\\data\\lesson001.dat";
+        try {
+            Process process = new ProcessBuilder( exePath, arg ).start();
+        } catch (IOException ex) {
+            Logger.getLogger(NowMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
